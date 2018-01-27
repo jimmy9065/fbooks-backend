031c8ca1c8357466f608b1c223fb30424177671e
Jimmy <jimmy9065@hotmail.com>
2018-01-27 01:56:16 -0800

    finish cleanUp

--- router/api.js
+++ router/api.js
@@ -15,18 +15,6 @@ router.get('/getpass', function(req, res){
   let encrypt = CryptoJS.AES.encrypt('123456',keyStr);
   let sencrypt = encrypt.toString(CryptoJS.enc.hex);
 
-  var tmp = base64url.encode(sencrypt);
-  console.log("coded version:")
-  console.log(tmp);
-  var tmp2 = base64url.decode(tmp);
-  console.log("decoded version:")
-  console.log(tmp2);
-
-  let date = CryptoJS.AES.decrypt(tmp2, keyStr);
-  let spassword = date.toString(CryptoJS.enc.Utf8);
-
-  console.log('decrypt password:'+spassword)
-
   res.status(200);
   res.send(base64url.encode(sencrypt));
 })
@@ -34,66 +22,44 @@ router.get('/getpass', function(req, res){
 
 router.get('/login', function(req, res){
   let username = req.query.username;
-  let password = req.query.password;
-
-  console.log("params")
-  console.log(password)
-
-  password = base64url.decode(password);
-  console.log("decoded")
-  console.log(password)
-
-  let date = CryptoJS.AES.decrypt(password, keyStr);
-
-  console.log('decrypted')
-  console.log(date)
-
-  let spassword = date.toString(CryptoJS.enc.Utf8);
-
-  console.log('into str')
-  console.log(spassword)
+  let decodedPassword = base64url.decode(req.query.password);
+  let decryptPassword = CryptoJS.AES.decrypt(decodedPassword, keyStr);
+  let password = decryptPassword.toString(CryptoJS.enc.Utf8);
  
+  response = {"pass": false, "cookie": ''};
   res.status(200);
   db.checkUser(username)
   .then((pass) =>{
-    if(pass == spassword)
-      res.send("correct");
-    else
-      res.send("wrong");
+    if(pass == password){
+      response.pass = true;
+      response.cookie = decodedPassword;
+    }
+    res.send(response);
   })
   .catch(() => {
-    res.send("wrong");
-  });
+    res.send(response);
+  })
 })
 
 router.post('/login', function(req, res){
   let username = req.body.username;
-  let password = req.body.password;
-
-  console.log(username)
-  console.log(password)
-
-  let date = CryptoJS.AES.decrypt(req.body.password, keyStr);
-
-  console.log('decrypted')
-  console.log(date)
-
-  let spassword = date.toString(CryptoJS.enc.Utf8);
-
-  console.log('into str')
-  console.log(spassword)
+  let decodedPassword = base64url.decode(req.body.password);
+  let password = CryptoJS.AES.decrypt(decodedPassword, keyStr)
+                         .toString(CryptoJS.enc.Utf8);
  
+  response = {"pass": true, "cookie": ''};
   res.status(200);
   db.checkUser(username)
   .then((pass) =>{
-    if(pass == spassword)
-      res.send("correct");
-    else
-      res.send("wrong");
+    if(pass == password){
+      response.pass = true;
+      response.cookie = decodedPassword;
+    }
+    res.send(response);
   })
   .catch(() => {
-    res.send("wrong");
-  });
+    res.send(response);
+  })
 })
 
 module.exports = router;
