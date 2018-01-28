7f645f8d1f7e89a0b8361ca6f13d045b151b2644
Jimmy <jimmy9065@hotmail.com>
2018-01-27 17:14:13 -0800

    add insert function

--- router/trans.js
+++ router/trans.js
@@ -6,13 +6,26 @@ router.put('/', function(req, res){
   console.log(req.cookies.BOOKSUID);
   console.log(req.body);
 
-  let username = req.cookies.BOOKSUID;
-  db.addTrans(req.body, username).then(() => {
-    res.sendStatus(201);
-  })
-  .catch(() => {
+  let cookie = req.cookies.BOOKSUID;
+  matches = cookie.match(/([^&]+)/ig);
+  console.log(matches);
+  if(cookie && matches && matches[0] && matches[1]){
+    username = matches[0];
+    aptID = matches[1];
+    db.addTrans(req.body, username, aptID)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log('error: put request:')
+      console.log('cookie: ' + cookie)
+      console.log('body:')
+      console.log(req.body)
+      res.sendStatus(400);
+    });
+  }
+  else
     res.sendStatus(400);
-  });
 })
 
 router.get('/allTrans', function(req, res){
